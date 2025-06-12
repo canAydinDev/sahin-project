9de8ba1d90be933ed0ab3b68ac56465ff12b27ee
canaydindev <canaydindev@gmail.com>
2025-06-12 20:52:17 +0300

    admin panel

--- src/lib/actions/appointment.actions.ts
+++ src/lib/actions/appointment.actions.ts
@@ -11,6 +11,7 @@ import {
 } from "@/lib/server/appwrite";
 
 import { parseStringify } from "../utils";
+import { Appointment } from "../../../types/appwrite.types";
 
 export const createAppointment = async (
   appointment: CreateAppointmentParams
@@ -38,6 +39,47 @@ export const getAppointment = async (appointmentId: string) => {
     );
 
     return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getRecentAppointmentList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")]
+    );
+
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointment) => {
+        if (appointment.status === "scheduled") {
+          acc.scheduledCount += 1;
+        } else if (appointment.status === "pending") {
+          acc.pendingCount += 1;
+        } else if (appointment.status === "cancelled") {
+          acc.cancelledCount += 1;
+        }
+
+        return acc;
+      },
+      initialCounts
+    );
+
+    const data = {
+      totalCount: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    };
+
+    return parseStringify(data);
   } catch (error) {
     console.log(error);
   }
---
+++ src/modules/home/ui/components/admin-panel/table/appointment-model/index.tsx
@@ -0,0 +1,3 @@
+export const ApoointmentModal = () => {
+  return <div>hello</div>;
+};
